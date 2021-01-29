fdb91270af895b45cad79ebfccab073eb8fe4930
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-01-29 11:49:32 +0100

    changed to mongoose, and start work with cookies

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
 const app = express();
@@ -14,14 +14,15 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('6012823e36ec0aee7c54a900')
+  User.findById('6013ba7b54b8e928ec9e1e34')
     .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
+      req.user = user;
       next();
     })
     .catch(err => console.log(err));
@@ -29,9 +30,32 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3005);
-});
+mongoose
+  .connect(
+    'mongodb+srv://kiko:12345@cluster0.u2uu1.mongodb.net/shop?retryWrites=true&w=majority', 
+    {useNewUrlParser: true, useUnifiedTopology: true}
+    )
+  .then(result => {
+    User
+      .findOne()
+      .then(user => {
+        if(!user) {
+          const user = new User({
+            name: 'Kiko',
+            email: 'kiko@kiko.com',
+            cart: {
+              items: []
+            }
+          });
+          user.save();
+        }
+      })
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
