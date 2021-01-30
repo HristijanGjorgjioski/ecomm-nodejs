7dcc22f371ca95f59850cfdded8111663d01e2ca
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-01-30 13:27:23 +0100

    working with cookies and session

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI =
+  'mongodb+srv://kiko:12345@cluster0.u2uu1.mongodb.net/shop';
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -18,15 +27,14 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use((req, res, next) => {
-  User.findById('6013ba7b54b8e928ec9e1e34')
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => console.log(err));
-});
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -36,24 +44,22 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    'mongodb+srv://kiko:12345@cluster0.u2uu1.mongodb.net/shop?retryWrites=true&w=majority', 
-    {useNewUrlParser: true, useUnifiedTopology: true}
+    MONGODB_URI,
+    { useNewUrlParser: true, useUnifiedTopology: true }
     )
   .then(result => {
-    User
-      .findOne()
-      .then(user => {
-        if(!user) {
-          const user = new User({
-            name: 'Kiko',
-            email: 'kiko@kiko.com',
-            cart: {
-              items: []
-            }
-          });
-          user.save();
-        }
-      })
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Kiko',
+          email: 'kiko@kiko.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
     app.listen(3000);
   })
   .catch(err => {
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,26 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
     path: '/login',
-    pageTitle: 'Login'
+    pageTitle: 'Login',
+    isAuth: false
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  User.findById('6013ba7b54b8e928ec9e1e34')
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      res.redirect('/');
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
   });
 };
