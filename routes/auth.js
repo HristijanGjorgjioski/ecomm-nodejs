931a7b65fac0d72d485b467dd9e72d13d7707964
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-02-02 10:05:19 +0100

    working on validation with express-validator

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const { check, body } = require('express-validator/check');
 
 const authController = require('../controllers/auth');
+const User = require('../models/user');
 
 const router = express.Router();
 
@@ -8,9 +10,51 @@ router.get('/login', authController.getLogin);
 
 router.get('/signup', authController.getSignup);
 
-router.post('/login', authController.postLogin);
+router.post(
+    '/login',
+    [
+      body('email')
+        .isEmail()
+        .withMessage('Please enter a valid email address.')
+        .normalizeEmail(),
+      body('password', 'Password has to be valid.')
+        .isLength({ min: 5 })
+        .isAlphanumeric()
+        .trim()
+    ],
+    authController.postLogin
+  );
 
-router.post('/signup', authController.postSignup);
+router.post(
+    '/signup', 
+    [
+    check('email')
+        .isEmail()
+        .withMessage('Please enter a valid e-mail')
+        .custom((value, { req }) => {
+            return User.findOne({ email: value })
+                .then(userDoc => {
+                if (userDoc) {
+                    return Promise.reject('Email already exsist, try another');
+                }
+            })
+        })
+        .normalizeEmail(), 
+        body('password', 'Password must have at least 5 charachters and contain only letters and numbers')
+            .isLength({ min:5 })
+            .isAlphanumeric()
+            .trim(),
+        body('confirmPassword')
+            .trim()
+            .custom((value, { req }) => {
+                if(value !== req.body.password) {
+                    throw new Error('Passwords must match')
+                }
+                return true;
+            })
+    ],
+    authController.postSignup
+);
 
 router.post('/logout', authController.postLogout);
 
